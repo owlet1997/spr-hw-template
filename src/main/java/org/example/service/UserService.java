9d489d2c9050b9f9aefb198891e88a696dd756ce
Nadezhda Shatalova <n.shatalova@haulmont.com>
2021-07-17 18:35:02 +0400

    fixes issue #3

--- src/main/java/org/example/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-import lombok.RequiredArgsConstructor;
-import org.example.repository.UserRepository;
-
-@RequiredArgsConstructor
-public class UserService {
-  private final UserRepository repository;
-}
